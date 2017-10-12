3da914c4edf9b63a250daf6bea7e07fa02f4cdcc
brickcap <akshatjiwan@gmail.com>
2017-10-12 16:36:09 +0530

    chaning the login title

--- static/ellobed.js
+++ static/ellobed.js
@@ -1,2 +1,6 @@
+jQuery(document).ready(function(){
+    var $ = jQuery;
+    $("#login>h1>a").attr("title","Enriching global hospitality ecosystem");
+    console.log("hello world");
+});
 
-console.log("hello world");
