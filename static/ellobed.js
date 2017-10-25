1917023d50255b6eeebad05e1c70f158dedc8046
ellobed <admin@ellobed.com>
2017-10-25 17:19:33 +0530

    Loading the remote document to the footer page

--- static/ellobed.js
+++ static/ellobed.js
@@ -3,6 +3,10 @@ jQuery(document).ready(function(){
     var attr_el = $("#login>h1>a");
     attr_el.attr("title","Enriching global hospitality ecosystem");
     attr_el.attr("href","/");
+    $("body").append("<div id='eb_main_footer'></div>");
+    $("#eb_main_footer").load("https://storage.googleapis.com/ellobed/static/footer.html #eb_main_footer",function(){
+	console.log("footer_loaded");
+    });
     console.log("hello world");
 });
 
