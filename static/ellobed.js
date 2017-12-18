183b77ea9f6eee31eb47f868ddfb3d7ac19a7a44
ellobed <admin@ellobed.com>
2017-12-18 20:32:20 +0530

    forgot the trailing slash in the match

--- static/ellobed.js
+++ static/ellobed.js
@@ -21,19 +21,26 @@ jQuery(document).ready(function(){
 	attr_el.attr("title","Enriching global hospitality ecosystem");
 	attr_el.attr("href","/");
 	$("body").append("<div id='eb_external_footer'></div>");
-	$("body").prepend("<div id='eb_external_header'></div>");
 	$("#eb_external_footer")
 	    .load("https://storage.googleapis.com/ellobed/static/footer.html #eb_main_footer",
 		  function(){
 		      console.log("footer_loaded");
 		  });
-	
+	if(url==="https://ellobed.com/"){
+	    //load the external header only on the front page
+	$("body").prepend("<div id='eb_external_header'></div>");
 	$("#eb_external_header")
 	    .load("https://s3-us-west-2.amazonaws.com/ellobed/header.html #eb_main_header",
 		  function(){
 		      console.log("header_loaded");
 		      $("body").show();
 		  });
+	}
+	else{
+	    console.log("not the right page baby");
+	    console.log(url);
+	    $("body").show();
+	}
 	
 	
     }
